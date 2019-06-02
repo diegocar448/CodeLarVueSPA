2b41537863342bc849bab02c82c3c1c2a24613d0
Diego Cardoso <diegocar448@hotmail.com>
2019-06-02 17:21:20 +0000

    paginate + filter home

--- resources/assets/js/components/frontend/pages/home/HomeComponent.vue
+++ resources/assets/js/components/frontend/pages/home/HomeComponent.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h1>Produtos</h1>
+
+        <search
+        @search="search"
+        ></search>
         <div class="row">
             <item 
             v-for="product in products.data" 
@@ -22,6 +26,7 @@
 <script>
 import PaginationComponent from "../../../layouts/PaginationComponent"
 import Item from "../../../layouts/Item"
+import SearchComponent from "../../../admin/layouts/SearchComponent"
 
 
 export default {
@@ -31,20 +36,37 @@ export default {
             this.$store.dispatch('loadProducts', {})
         }
     },
+    data(){
+        return{
+            filter:''
+        }
+    },
     computed:{
         products(){
             return this.$store.state.products.items
+        },
+        params(){
+            return{
+                filter:this.filter,
+                page:this.products.current_page,
+            }
         }
     },
     methods:{
         loadProducts(page = 1)
         {
-            this.$store.dispatch('loadProducts', {page})
+            this.$store.dispatch('loadProducts', {...this.params, page})
+        },
+        search(filter){
+            this.filter = filter
+
+            this.loadProducts(filter)
         }
     },
     components: {
         paginate: PaginationComponent,
-        Item
+        Item,
+        search: SearchComponent
     }
     
 }
