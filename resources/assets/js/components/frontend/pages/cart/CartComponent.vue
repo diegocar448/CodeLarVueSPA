2962df6318a5bbba42388d0e6428b364a677dc9e
diegocar448 <diegocar448@hotmail.com>
2019-06-03 13:33:58 -0300

    remover produtos do carrinho/cart

--- resources/assets/js/components/frontend/pages/cart/CartComponent.vue
+++ resources/assets/js/components/frontend/pages/cart/CartComponent.vue
@@ -1,5 +1,49 @@
 <template>
     <div>
         <h1>Carrinho de compras</h1>
+
+        <div class="row">
+            <div class="col-12" v-for="product in cart.products" :key="product.id">
+                <div class="col-3">
+                    <div v-if="product.image">
+                    <img :src="[`storage/products/${product.image}`]" :alt="product.name" class="img-list">
+                    </div> 
+                    <div v-else>
+                        <img src="/imgs/no-image.jpg" :alt="product.name" class="img-list" alt="">
+                    </div>
+                    <div class="col-7">
+                        {{ product.name }}
+                    </div>
+                    <div class="col-2">
+                        <button class="btn btn-danger" @click.prevent="removeCart(product)">
+                            Remover Carrinho
+                        </button>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    computed:{
+        cart(){
+            return this.$store.state.cart
+        }
+    },
+    methods:{
+        removeCart(product){
+            this.$store.commit('REMOVE_PRODUCT_CART', product)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.img-list{
+    max-width:100px;
+}
+</style>
+
